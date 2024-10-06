6c58810a7512bbe4e3e89fee9da44536b2e26e26
Mehmet <matayurt01@gmail.com>
2024-10-06 15:42:46 +0300

    Homepage-and-DBConnection

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,7 @@
 import '@styles/globals.css'
+//COMPONENTS
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
     title: "Promptopia",
@@ -9,13 +12,16 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient'/>
             </div>
 
             <main className='app'>
+                <Nav />
                 {children}
             </main>
+            </Provider>
         </body>
     </html>
   )
---
+++ utils/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // track db connection status
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+};
